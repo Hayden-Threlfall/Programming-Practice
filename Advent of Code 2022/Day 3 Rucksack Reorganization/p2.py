859c88e5ddf363ddf6bbf979bf745803ba90ec21
Hayden-Threlfall <Hayden.t.threlfall@gmail.com>
2022-12-03 15:47:57 -0600

    Day 2 Complete

--- Advent of Code 2022/Day 3 Rucksack Reorganization/p2.py
+++ Advent of Code 2022/Day 3 Rucksack Reorganization/p2.py
@@ -8,27 +8,17 @@ def priority(ch):
         num =  (ord(ch) - 65) + 27
     return num
 
-temp = 0
+temp = ""
 count = 0
-str = input[0].replace("\n","")
-count = 0
-end = True
 i = 0
 con = 0
 index = int(len(input))
-index = 12
 while(i<index):
     for ch in input[i].replace("\n",""):
-        for j in range(3):
-            temp = input[i+j].replace("\n","")
-            #print((i+j))
-            if(temp.find(ch) >= 0):
-                count += 1
-        if(count == 3):
+        if(ch in input[i+1].replace("\n","") and ch in input[i+2].replace("\n","")):
+            print(ch)
+            con += priority(ch)
             break
-        count = 0
     i += 3
-    print(ch)
-    con += priority(ch)
 print(con)
 
