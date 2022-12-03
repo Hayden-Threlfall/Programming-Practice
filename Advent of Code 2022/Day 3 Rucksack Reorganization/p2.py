c44c196773995f9e5bb25fb7de967d99460bd426
Hayden-Threlfall <Hayden.t.threlfall@gmail.com>
2022-12-03 15:29:12 -0600

    Day 2 part 2 not working

--- Advent of Code 2022/Day 3 Rucksack Reorganization/p2.py
+++ Advent of Code 2022/Day 3 Rucksack Reorganization/p2.py
@@ -1,33 +1,34 @@
-with open('input-small.txt') as f:
+with open('input.txt') as f:
     input = f.readlines()
     
 def priority(ch):
     if (ord(ch) > 96):
         num =  (ord(ch) - 97) + 1
-        print()
     else:
         num =  (ord(ch) - 65) + 27
     return num
 
 temp = 0
 count = 0
-
 str = input[0].replace("\n","")
-for ch in str:
-    
-    print()
-
-
-
-count = 1
-for ch in str:
-    #print(ch)
-    for i in range(len(input)-1):
-        input[i+1]
-        if(input[i+1].find(ch) >= 0):
-            count += 1
-            i += 1
-    if(count == 3):
-        break
-print(ch)
+count = 0
+end = True
+i = 0
+con = 0
+index = int(len(input))
+index = 12
+while(i<index):
+    for ch in input[i].replace("\n",""):
+        for j in range(3):
+            temp = input[i+j].replace("\n","")
+            #print((i+j))
+            if(temp.find(ch) >= 0):
+                count += 1
+        if(count == 3):
+            break
+        count = 0
+    i += 3
+    print(ch)
+    con += priority(ch)
+print(con)
 
