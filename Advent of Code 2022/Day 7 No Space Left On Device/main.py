a44010d89419041b7b4f6c1edb7e3db9471a5cc6
Hayden-Threlfall <Hayden.t.threlfall@gmail.com>
2022-12-19 22:05:07 -0600

    Day 7 Still broke

--- Advent of Code 2022/Day 7 No Space Left On Device/main.py
+++ Advent of Code 2022/Day 7 No Space Left On Device/main.py
@@ -2,31 +2,39 @@ with open('input-small.txt') as f:
     input = f.readlines()
 arr = []
 i = -1
+l = 0
+count = 0
+
 for str in input:
     temp = str.replace("\n","").rsplit(" ")
     if(str.count("$ cd ")>0):
-        arr.append([])
-        i +=1
-        arr[i].append(temp[2])
+        if(temp[2]==".."):
+            l-=1
+        else:
+            arr.append([])
+            i +=1
+            arr[i].append(l)
+            l+=1
         
     if(temp[0].isdigit()):
         arr[i].append(int(temp[0]))
-print(arr)
-
 #Find size less than 100000
 count = 0
 fCount=0
-for i in arr:
-    for j in range(1, len(i)):
-        count += i[j]
+previous = 0
+for i in range(len(arr)):
+    #Find Dir size
+    if(previous > arr[i][0]):
+        print("IDK")
+    else:
         
-        
-#####The problem with this code is I count the files i the directory, I need to count the other directories that come after too.
+        for j in range(1, len(arr[i])):
+            count += arr[i][j]
+            if(count>100000):
+                break
+        ###END
+        if(count<=100000):
+                fCount += count
+        count = 0
 
-
-if(count<=100000):
-        fCount += count
-    count = 0
-    
-        
 print(fCount)
