ecd0f35df9294be23ed420267babf68c4769949a
Hayden-Threlfall <Hayden.t.threlfall@gmail.com>
2022-12-20 23:44:08 -0600

    Still on day 8
    Stuck on python arrays and how some pointers work behind the scenes. Taking more code then it should to define and use arrays easier in other languages.

--- Advent of Code 2022/Day 8 Treetop Tree House/main.py
+++ Advent of Code 2022/Day 8 Treetop Tree House/main.py
@@ -1,43 +1,53 @@
-with open('input.txt') as f:
+import numpy as np
+
+with open('input-small.txt') as f:
     input = f.readlines()
 for i in range(len(input)):
     input[i] = input[i].replace("\n","")
+arr = []
+for i in range(len(input)):
+    arr.append([])
+    for j in range(len(input[0])):
+        arr[i].append(int(input[i][j]))
+        
 seen = 0
 seen += len(input)*2
 seen += (len(input[0])*2)-4
 
+
+
 temp = 0
-lenj = len(input[0])
-for i in range(1, len(input)-1):
+lenj = len(input[0])-1
+
+bol = [[True]*lenj+1]*lenj+1 ##True or false if this tree has been found
+for i in range(lenj):
+    bol[0][i] = False
+    bol[i][0] = False
+    bol[lenj-i][i] = False
+    bol[j][lenj-j] = False
+print(bol)
+        
+for i in range(1, lenj):
     temp = input[i][0]
     for j in range(1, lenj):
-        if(int(temp) < int(input[i][j])):
+        if(int(temp) < arr[i][j]):
             seen+=1
-            temp=int(input[i][j])
-        else:
-            break
-for i in range(1, len(input)-1):
-    temp = input[i][0]
-    for j in range(1, lenj):
-        if(int(temp) < int(input[i][lenj-j])):
-            seen+=1
-            temp=int(input[i][lenj-j])
+            #print(arr[i][j])
+            temp=arr[i][j]
+        elif(int(temp) <= arr[i][j]):
+            seen+=0
         else:
             break
 for i in range(1, lenj):
     temp = input[i][0]
-    for j in range(1, len(input)-1):
-        if(int(temp) < int(input[i][j])):
+    for j in range(1, lenj):
+        if(int(temp) < arr[i][lenj-j]):
             seen+=1
-            temp=int(input[i][j])
+            print(arr[i][j])
+            temp=arr[i][j]
+        elif(int(temp) <= arr[i][lenj-j]):
+            seen+=0
         else:
-            break
-for i in range(1, lenj):
-    temp = input[i][0]
-    for j in range(1, len(input)-1):
-        if(int(temp) < int(input[i][lenj-j])):
-            seen+=1
-            temp=int(input[i][lenj-j])
-        else:
-            break
+            break    
+print(np.matrix(arr))
 print(seen)
---
+++ Advent of Code 2022/Day 8 Treetop Tree House/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+lenj = 5
+bol = np.array
+print(bol)
+bol[4][1] = False 
+print(bol)
